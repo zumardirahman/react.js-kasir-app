3b1ccad693f229de3eb1cdd5be3831d0c91814fc
zuMar <zumar2014@gmail.com>
2022-02-16 15:29:56 +0700

    16022022.1529

---
+++ src/components/Index.js
@@ -0,0 +1,6 @@
+import ComNavbar from './ComNavbar'
+import ComLeftBar from './ComLeftBar'
+import ComRightBar from './ComRightBar'
+import Menus from './Menus'
+
+export {ComNavbar, ComLeftBar, ComRightBar, Menus}
